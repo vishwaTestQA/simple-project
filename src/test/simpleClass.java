a998b91d0bc39f6a27f6f024d241887b4c9e6b13
vishwa <vishwa80123@gmail.com>
2023-05-31 13:51:30 +0530

    2nd-first

--- src/test/simpleClass.java
+++ src/test/simpleClass.java
@@ -4,6 +4,12 @@ public class simpleClass {
 static int i=0;
 public static void main(String ...args) {
 	System.out.println(i);
+	System.out.println(i+1);
 	System.out.println(i);
+	System.out.println(i+20);
+	System.out.println(i+30);
+	System.out.println(i+50);
+	System.out.println(i+40);
+	System.out.println(i+60);
 }
 }
